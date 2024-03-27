6000327570bacfb63c3e64a31d68f3466e4ec024
Lars Kemper <lk297162@fh-muenster.de>
2024-03-27 16:59:59 +0100

    feat: add example for 'str_cat' task

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lib/http-lib/httplib.h"
+
+// Praktikum 1 "str_cat"-Task example
+int main() {
+    string* str1 = cpy_str("Hello, ", 7);
+    string* str2 = cpy_str("world!", 7);
+
+    str_cat(str1, get_char_str(str2), get_length(str2));
+
+    print_string(str1);
+    putchar('\n');
+
+    free_str(str1);
+    free_str(str2);
+
+    return 0;
+}
