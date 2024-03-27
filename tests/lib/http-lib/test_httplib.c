508a8665d4d12cee1a85b02b0f7ac46b14a405bc
Lars Kemper <lk297162@fh-muenster.de>
2024-03-27 20:47:37 +0100

    feat: compile to build directory

---
+++ tests/lib/http-lib/test_httplib.c
@@ -0,0 +1,25 @@
+#include "../../../lib/unity/src/unity.h"
+
+void setUp()
+{
+
+}
+
+void tearDown()
+{
+
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    /*
+     * Add in between
+     * RUN_TEST(func_name);
+     */
+
+    UNITY_END();
+
+    return 0;
+}
