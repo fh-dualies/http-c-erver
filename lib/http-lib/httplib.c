74fef77244bd54bf3d83d5d25441e4bdd8dc607c
Manuel Grave <mg573007@fh-muenster.de>
2024-03-27 14:02:15 +0000

    build: add http-lib

---
+++ lib/http-lib/httplib.c
@@ -0,0 +1,55 @@
+#include <assert.h>
+#include "httplib.h"
+
+string* str_cat(string* dest, const char* src, size_t len) {
+    printf("TODO!");
+    return dest;
+}
+
+string* new_string(size_t len) {
+    string* str = calloc(sizeof(string), 1);
+    if(str == NULL) {
+        exit(2);
+    }
+
+    str->str = calloc(len, 2);
+    if(str->str == NULL) {
+        exit(3);
+    }
+
+    str->len = len * 2;
+    str->pos = 0;
+
+    return str;
+}
+
+void print_string(string* str) {
+    for(int i=0; i <= str->pos; i++) {
+        putchar(str->str[i]);
+    }
+}
+
+string* cpy_str(const char* src, size_t len) {
+    string* dest = new_string(len);
+    memcpy(dest->str, src, len);
+    dest->pos=len-1;
+
+    return dest;
+}
+
+void free_str(string* str) {
+    assert(str != NULL);
+    assert(str->str != NULL);
+    free(str->str);
+    free(str);
+}
+
+size_t get_length(string* str) {
+    assert(str != NULL);
+    return str->pos + 1;
+}
+
+char* get_char_str(string* str) {
+    assert(str != NULL);
+    return str->str;
+}
---
+++ lib/http-lib/httplib.h
@@ -0,0 +1,26 @@
+//
+// Created by Sebastian Schinzel on 25.03.20.
+//
+
+#ifndef ECHO_SERVER_HTTPLIB_H
+#define ECHO_SERVER_HTTPLIB_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct string {
+    size_t len;
+    size_t pos;
+    char* str;
+};
+typedef struct string string;
+
+string* str_cat(string* dest, const char* src, size_t len);
+string* new_string(size_t );
+void print_string(string* str);
+string* cpy_str(const char* src, size_t len);
+void free_str(string* str);
+size_t get_length(string* str);
+char* get_char_str(string* str);
+
+#endif //ECHO_SERVER_HTTPLIB_H
