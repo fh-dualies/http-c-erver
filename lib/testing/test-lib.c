1671354e0cb529ce1825dc9df35f429434175933
Dennis Höllmann <dh687982@fh-muenster.de>
2024-03-31 11:58:39 +0200

    test: add basic testing lib

---
+++ lib/testing/test-lib.c
@@ -0,0 +1,49 @@
+#include "test-lib.h"
+
+void error_color() {
+  printf("\033[31m");
+}
+
+void success_color() {
+  printf("\033[32m");
+}
+
+void reset_color() {
+  printf("\033[0m");
+}
+
+
+void expect_equal(string *str, size_t expected_len, const char *expected_str) {
+  string *expected = cpy_str(expected_str, expected_len);
+  if (str->len != expected->len) {
+    error_color();
+    printf("Expected length %zu, got %zu\n", expected->len, str->len);
+    reset_color();
+
+    free_str(expected);
+    return;
+  }
+
+  for (size_t i = 0; i < str->len; i++) {
+    if (str->str[i] != expected->str[i]) {
+      error_color();
+      printf("Expected:");
+      print_string(expected);
+      printf("\nGot:");
+      print_string(str);
+      reset_color();
+
+      free_str(expected);
+      return;
+    }
+  }
+
+  success_color();
+  print_string(str);
+  printf(" == ");
+  print_string(expected);
+  printf("\n");
+  reset_color();
+
+  free_str(expected);
+}
---
+++ lib/testing/test-lib.h
@@ -0,0 +1,12 @@
+//
+// Created by dennis on 31/03/24.
+//
+
+#ifndef PSE_2024_TEST_LIB_H
+#define PSE_2024_TEST_LIB_H
+
+#include "../http-lib/httplib.h"
+
+void expect_equal(string *str, size_t expected_len, const char *expected_str);
+
+#endif //PSE_2024_TEST_LIB_H
