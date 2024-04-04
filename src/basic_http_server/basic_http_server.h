ae062007342a83fe2aa8d9fa61034e61d2a54e60
Lars Kemper <lk297162@fh-muenster.de>
2024-04-04 22:33:37 +0200

    feat: make echo server optional

---
+++ src/basic_http_server/basic_http_server.h
@@ -0,0 +1,9 @@
+#ifndef BASIC_HTTP_SERVER_H
+#define BASIC_HTTP_SERVER_H
+
+/// @brief Start the basic http server.
+/// @param request The request.
+/// @return The response.
+string* basic_http_server(string* request);
+
+#endif
