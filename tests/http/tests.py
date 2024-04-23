fae34ab03507cc53f9b4d23864b1b70965415f9f
Dennis Höllmann <dh687982@fh-muenster.de>
2024-04-23 19:46:43 +0200

    feat: add more http test

--- tests/http/tests.py
+++ tests/http/tests.py
@@ -28,27 +28,111 @@ if len(sys.argv) == 3:
 # Initialise Laz0rCannon
 cannon = Laz0rCannon(host=host, port=port)
 
-# Laz0r Beams (Tests to fire against the server).
+#
+# Methods
+#
 cannon += Beam(
-    description='Ungültiges Http-Verb "POST"',
-    request='POST /index.html HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    description='No HTTP method specified',
+    request=' /debug HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 400']
+)
+cannon += Beam(
+    description='HTTP method with lowercase letters',
+    request='get /debug HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
     response=['HTTP/1.1 501']
 )
 cannon += Beam(
-    description='Http-Verb in Kleinbuchstaben (get)',
-    request='get /index.html HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    description='Not implemented HTTP method',
+    request='POST /debug HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
     response=['HTTP/1.1 501']
 )
+
+#
+# HTTP-Version
+#
 cannon += Beam(
-    description='Keine Ressource angegeben',
+    description='No HTTP-Version specified',
+    request='GET /debug\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 400']
+)
+cannon += Beam(
+    description='HTTP-Version with lowercase letters',
+    request='GET /debug http/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 400']
+)
+cannon += Beam(
+    description='HTTP-Version 1.0',
+    request='GET /debug HTTP/1.0\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 200']
+)
+cannon += Beam(
+    description='HTTP-Version 1.1',
+    request='GET /debug HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 200']
+)
+cannon += Beam(
+    description='HTTP-Version 2.0',
+    request='GET /debug HTTP/2.0\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 505']
+)
+
+#
+# Ressources
+#
+cannon += Beam(
+    description='No ressource specified',
     request='GET  HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
     response=['HTTP/1.1 400']
 )
 cannon += Beam(
-    description='Keine Http-Version angegeben',
-    request='GET /\r\nHost: {host}:{port}\r\n\r\n',
+    description='Unicode ressource',
+    request='GET /debugß*#+ HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 404']
+)
+cannon += Beam(
+    description='ressource outside of document root',
+    request='GET /../../debug HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 404']
+)
+
+#
+# Host
+#
+
+# TODO: Enable these tests once the server correctly handles the Host header
+
+#cannon += Beam(
+#    description='No Host specified',
+#    request='GET /debug HTTP/1.1\r\n\r\n',
+#    response=['HTTP/1.1 400']
+#)
+#cannon += Beam(
+#    description='Host with lowercase letters',
+#    request='GET /debug HTTP/1.1\r\nhost: {host}:{port}\r\n\r\n',
+#    response=['HTTP/1.1 400']
+#)
+# TODO: Add more tests for Host
+
+#
+# Miscellaneous
+#
+cannon += Beam(
+    description='Request with multiple spaces',
+    request='GET /debug  HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
     response=['HTTP/1.1 400']
 )
+cannon += Beam(
+    description='Request with unicode characters',
+    request='GET /debug HTTP/1.1\r\nHostß*#+: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 200']
+)
+cannon += Beam(
+    description='[OPTIONAL] favicon.ico (depends on htdocs folder)',
+    request='GET /favicon.ico HTTP/1.1\r\nHost: {host}:{port}\r\n\r\n',
+    response=['HTTP/1.1 200']
+)
+
+
 
 # Pew pew!
 successful = cannon.pewpew()
