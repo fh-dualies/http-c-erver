6ad10b94f747cd3a1a1d3dcd590b55b819b59247
Lars Kemper <lk297162@fh-muenster.de>
2024-04-25 10:01:36 +0200

    feat: sort string_lib function by string lifecycle

--- lib/string_lib/string_lib.h
+++ lib/string_lib/string_lib.h
@@ -19,20 +19,15 @@ struct string {
 void *exit_err(const char *function_name, const char *reason);
 
 /**
- * @brief Concatenate a string to another string
- * @warning The destination will be reallocated
+ * @brief Create a new string
  * @warning This function will add a null terminator to the end of the string
  *
- * Returns NULL if the destination string is NULL.
- * Returns the destination string if the source string is NULL or len is 0.
- * Exits with code 1 if the memory allocation fails.
+ * Exits with code 1 if the memory allocation of the string struct fails or if the memory allocation
+ * of the string fails.
  *
- * @param dest The destination string
- * @param src The source string
- * @param len The length of the source string
- * @return The destination string
+ * @return The new string
  */
-string *str_cat(string *dest, const char *src, size_t len);
+string *_new_string();
 
 /**
  * @brief Set a string to another string by freeing the destination string and allocating memory for
@@ -50,6 +45,36 @@ string *str_cat(string *dest, const char *src, size_t len);
  * @return
  */
 string *str_set(string *dest, const char *src, size_t len);
+
+/**
+ * @brief Concatenate a string to another string
+ * @warning The destination will be reallocated
+ * @warning This function will add a null terminator to the end of the string
+ *
+ * Returns NULL if the destination string is NULL.
+ * Returns the destination string if the source string is NULL or len is 0.
+ * Exits with code 1 if the memory allocation fails.
+ *
+ * @param dest The destination string
+ * @param src The source string
+ * @param len The length of the source string
+ * @return The destination string
+ */
+string *str_cat(string *dest, const char *src, size_t len);
+
+/**
+ * @brief Copy a string
+ * @warning The destination will be reallocated
+ * @warning This function will add a null terminator to the end of the string
+ *
+ * Exits with code 1 if the memory allocation of the string struct fails or if the memory allocation
+ * of the string fails.
+ *
+ * @param src The source string
+ * @param len The length of the source string
+ * @return The copied string
+ */
+string *cpy_str(const char *src, size_t len);
 
 /**
  * @brief Compare two strings
@@ -74,63 +99,6 @@ int str_cmp(string *str1, const char *str2);
 void str_to_lower(string *input);
 
 /**
- * @brief Create a new string
- * @warning This function will add a null terminator to the end of the string
- *
- * Exits with code 1 if the memory allocation of the string struct fails or if the memory allocation
- * of the string fails.
- *
- * @return The new string
- */
-string *_new_string();
-
-/**
- * @brief Print a string
- *
- * @param str The string to print
- */
-void print_string(string *str);
-
-/**
- * @brief Copy a string
- * @warning The destination will be reallocated
- * @warning This function will add a null terminator to the end of the string
- *
- * Exits with code 1 if the memory allocation of the string struct fails or if the memory allocation
- * of the string fails.
- *
- * @param src The source string
- * @param len The length of the source string
- * @return The copied string
- */
-string *cpy_str(const char *src, size_t len);
-
-/**
- * @brief Free a string
- *
- * Returns if the string is NULL.
- *
- * @param str The string to free
- */
-void free_str(string *str);
-
-/**
- * @brief Get the length of a string
- *
- * @param str The string
- * @return The length of the string
- */
-size_t get_length(string *str);
-
-/**
- * @brief Get the value of a string
- *
- * @param str The string
- * @return The value of the string
- */
-char *get_char_str(string *str);
-
-/**
  * @brief Convert a size_t to a string
  * @waring The return value must be freed after use
  *
@@ -151,5 +119,37 @@ string *size_t_to_string(size_t num);
  * @return The string
  */
 string *int_to_string(int num);
+
+/**
+ * @brief Print a string
+ *
+ * @param str The string to print
+ */
+void print_string(string *str);
+
+/**
+ * @brief Get the length of a string
+ *
+ * @param str The string
+ * @return The length of the string
+ */
+size_t get_length(string *str);
+
+/**
+ * @brief Get the value of a string
+ *
+ * @param str The string
+ * @return The value of the string
+ */
+char *get_char_str(string *str);
+
+/**
+ * @brief Free a string
+ *
+ * Returns if the string is NULL.
+ *
+ * @param str The string to free
+ */
+void free_str(string *str);
 
 #endif
